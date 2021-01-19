25da441858cd4180b78e60f3226750e953fab74f
yoghurt1131 <yoghurt1131@gmail.com>
2021-01-19 22:58:39 +0900

    restructure main directory

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './main/assets/index.css';
 import reportWebVitals from './reportWebVitals';
-import { App } from './App';
+import { App } from './main/App';
 
 ReactDOM.render(
   <React.StrictMode>
