94994baa19e79b94646b7e483603bb6e10aae04f
yoghurt1131 <yoghurt1131@gmail.com>
2021-01-19 23:03:30 +0900

    add jest settings and simple test

---
+++ src/test/foo.test.ts
@@ -0,0 +1,8 @@
+import { PokemonRepository } from "../main/repository/PokemonRepository";
+test('basic', () => {
+  expect(1+1).toBe(2);
+});
+
+test('basic again', () => {
+  expect(1+2).toBe(3);
+});
